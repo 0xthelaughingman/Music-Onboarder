e547dcb9b8b16dca1f8d3383fd28697614137c12
Anil Kumar <anilkumar.jan28@gmail.com>
2020-04-22 01:15:26 +0530

    Spelling correction  On branch improved-dir-handler  Your branch is up to date with 'origin/improved-dir-handler'.  Changes to be committed: 	modified:   DirHandler.py

--- DirHandler.py
+++ DirHandler.py
@@ -3,13 +3,13 @@ import os
 
 class DirHandler(object):
     def __init__(self, path):
-        self.__midea_files = []
+        self.__media_files = []
         if (self.__is_valid_path(path)):
-            self.__search_midea_files(path)
+            self.__search_media_files(path)
 
-    def __search_midea_files(self, path):
-        if(self.__is_midea(path)):
-            self.__midea_files.append(path)
+    def __search_media_files(self, path):
+        if(self.__is_media(path)):
+            self.__media_files.append(path)
             return
 
         if(not self.__is_dir(path)):
@@ -17,24 +17,24 @@ class DirHandler(object):
         
         dir_items = os.listdir(path)
         for item in dir_items:
-            self.__search_midea_files("%s/%s" %(path,item))
+            self.__search_media_files("%s/%s" %(path,item))
 
     def __is_dir(self, path):
         return os.path.isdir(path)
     
-    def __is_midea(self, path):
-        return path.endswith("mp3") or path.endswith(".mp4")
+    def __is_media(self, path):
+        return path.endswith("py") or path.endswith(".mp4")
     
     def __is_valid_path(self, path):
         return os.path.exists(path)
 
     def get_files(self):
-        return self.__midea_files
+        return self.__media_files
 
 
 if __name__ == "__main__":
-    midea_files = DirHandler("path").get_files()
+    media_files = DirHandler("path").get_files()
     counter = 0;
-    for file in midea_files:
+    for file in media_files:
         counter += 1
         print("%06d - %s" %(counter, file))
