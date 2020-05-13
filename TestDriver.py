dba1eac2ab5f009347cfccddb36dea17283e4cc9
Cruzo007 <csoni.chirag@gmail.com>
2020-05-13 22:43:37 +0530

    Cleanup

--- TestDriver.py
+++ TestDriver.py
@@ -12,6 +12,6 @@ from source.utils.loggerHelper import LoggingHelper
 if __name__ == "__main__":
     logger_def = LoggingHelper.setup_logger()
     ob = AmazonMusicGetter("https://music.amazon.in/user-playlists/24c50364686646d29c50c237d7abf1f8i8n0?ref=dm_sh_5f15-f02c-7958-1c3b-b801b")
-    ob = SpotifySetter(True, ob.get_asset_list(), "email", "#Hashtagderp1234", "TEST")
+    ob = SpotifySetter(True, ob.get_asset_list(), "email", "password", "TEST")
 
 
